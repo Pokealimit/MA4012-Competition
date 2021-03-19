66a144d66b790ad502e831ed1a6c0acb63cc5475
Pokealimit <78751410+Pokealimit@users.noreply.github.com>
2021-03-19 22:10:10 +0800

    Initial Commit

---
+++ encoder_updated.c
@@ -0,0 +1,159 @@
+#ifndef __ENCODER__
+#define __ENCODER__
+
+#include "var_updated.c"
+
+/* Print coordinates and heading onto bluetooth module*/
+void print_bluetooth(int choice){
+	// 1 - Print current coordinates
+	char coord[80];
+	switch(choice){
+		case 1: sprintf(coord, "X = %.0f, Y = %.0f, heading = %.0f \n",odom.X,odom.Y,odom.heading);
+  					bnsSerialSend(UART1, coord);
+  					break;
+  	//case 2: sprintf(coord, "Left count = %d, Left count period = %f",leftcounter,count_period_left/1000.0);
+	}
+}
+
+/* This task counts the number of times that the reflective
+sensor detects a change from 0 to 1, signifying that the
+black strip is crossed and the wheel has completed half a
+revolution. As it is counting, it also updates the time
+difference between each count, stored under 'count_period_left'
+and 'count_period_right'. */
+task counting(){
+	int leftRead = 0;		//Variable to identify previously read state
+	int rightRead = 0;  //Variable to identify previously read state
+	static int leftcounter = 0;		//Number of times left strip is crossed
+	static int rightcounter =0;		//Number of times right strip is crossed
+	float current_time_left;
+	float current_time_right;
+	current_time_left = nSysTime;
+	current_time_right = nSysTime;
+
+	while(true){
+		if(SensorValue[leftWheelSensor]==1 && leftRead==0){
+			leftRead=1;
+			leftcounter++;
+			if(leftcounter>1){ 		//To eliminate error due to starting position of the encoder, first count is ignored
+				count_period_left = nSysTime - current_time_left;
+				current_time_left = nSysTime;
+			}
+			else current_time_left = nSysTime;
+			//writeDebugStreamLine("Left count = %d, Left count period = %f",leftcounter,count_period_left/1000.0);
+		}
+		else if(SensorValue[leftWheelSensor]==0 && leftRead==1){
+			leftRead=0;
+		}
+		if(SensorValue[rightWheelSensor]==1 && rightRead==0){
+			rightRead=1;
+			rightcounter++;
+			if(rightcounter>1){		//To eliminate error due to starting position of the encoder, first count is ignored
+				count_period_right = nSysTime - current_time_right;
+				current_time_right = nSysTime;
+			}
+			else current_time_right = nSysTime;
+			//writeDebugStreamLine("Right count = %d, Right count period = %f",rightcounter,count_period_right/1000.0);
+		}
+		else if(SensorValue[rightWheelSensor]==0 && rightRead==1){
+			rightRead=0;
+		}
+		if(motor[leftMotor] == 0 || motor[rightMotor] == 0){
+			leftcounter = 0;
+			rightcounter = 0;
+		}
+	}
+}
+
+
+task mapping{
+	//interval is in milliseconds
+	float interval=10;
+
+	while(true){
+		/* Kinematics Equations:
+		w_left = 1/(time taken for left wheel to complete half a revolution) * PI rad/s
+		w_right = 1/(time taken for right wheel to complete half a revolution) * PI rad/s
+		v_X = w_left + w_right * R/2 * cos(heading in rad)
+		v_Y = w_left + w_right * R/2 * sin(heading in rad)
+		w = (w_right - w_left) * R/L
+		where R is the radius of wheel and L is the distance between each wheel */
+		if (motor[leftMotor] > 0 && count_period_left > 0)
+			odom.w_left = 1.0 / (count_period_left/1000.0) * PI;
+		else if(motor[leftMotor] < 0 && count_period_left > 0)
+			odom.w_left = - 1.0 / (count_period_left/1000.0) * PI;
+		else odom.w_left = 0;
+		if (motor[rightMotor] > 0 && count_period_right > 0)
+			odom.w_right = 1.0 / (count_period_right/1000.0) * PI;
+		else if(motor[rightMotor] < 0 && count_period_left > 0)
+			odom.w_right = - 1.0 / (count_period_right/1000.0) * PI;
+		else odom.w_right = 0;
+		odom.v_X = (odom.w_left + odom.w_right) * 3.5/2.0 * cos(odom.heading/180*PI);
+		odom.v_Y = (odom.w_left + odom.w_right) * 3.5/2.0 * sin(odom.heading/180*PI);
+		odom.w = (odom.w_right - odom.w_left) * 3.5/25.0;
+
+		/* Updating x,y coordinates and heading
+		At every interval, the odometry of the robot is updated by
+		taking the current X, Y and heading values and adding them to
+		the change (v_X,v_Y,w respectively) multiplied by the interval time */
+		if (count_period_left > 0 && count_period_right > 0){
+			odom.X = odom.X + odom.v_X * interval/1000.0 * 1.225;		//scaling factor of 1.225 from testing
+			odom.Y = odom.Y + odom.v_Y * interval/1000.0 * 1.225;		//scaling factor of 1.225 from testing
+			// if facing West (337.5 - 22.5 degrees)
+			if (odom.compass_heading == 0){
+				if(odom.heading<=odom.compass_heading + 22.5 && odom.heading>=odom.compass_heading - 22.5)
+				odom.heading = odom.heading + odom.w * 180 / PI * interval/1000.0;
+				else if (odom.heading > 180 && odom.heading < 337.5)
+					odom.heading = 337.5;
+				else if (odom.heading < 180 && odom.heading > 22.5)
+					odom.heading = 22.5;
+			}
+			// if not facing West
+			else {
+				if(odom.heading<=odom.compass_heading + 22.5 && odom.heading>=odom.compass_heading - 22.5)
+					odom.heading = odom.heading + odom.w * 180 / PI * interval/1000.0;
+				else if (odom.heading < odom.compass_heading - 22.5)
+					odom.heading = odom.compass_heading - 22.5;
+				else if (odom.heading > odom.compass_heading + 22.5)
+					odom.heading = odom.compass_heading + 22.5;
+			}
+
+			if (odom.heading >= 360)
+				odom.heading = odom.heading - 360;
+			if (odom.heading < 0)
+				odom.heading = odom.heading + 360;
+			writeDebugStreamLine("X = %f, Y = %f, heading = %f",odom.X,odom.Y,odom.heading);
+			print_bluetooth(1);
+		}
+
+
+		sleep(interval);
+	}
+}
+
+task compass_tracking{
+	// to track compass state
+	float previous_state = compass();
+
+	/* Determine compass reading based on values read from Digital Ports 8-11
+Values read from Ports 8-11 correspond to a 4-bit binary number
+Decimal value = MSB*2^3 + Bit2*2^2 + Bit3*2^1 + LSB
+Decimal value corresponds to a compass direction as given by data sheet */
+while(true){
+	odom.compass_heading = compass();
+	//writeDebugStreamLine("Previous state: %.2f", previous_state);
+	//writeDebugStreamLine("Current state: %.2f", odom.compass_heading);
+
+	if (previous_state!=odom.compass_heading){
+		if ((previous_state == 0 && odom.compass_heading == 315) || (odom.compass_heading == 0 && previous_state == 315)){
+			odom.heading = 337.5;
+		}
+		else odom.heading = (odom.compass_heading + previous_state) / 2;
+		previous_state = odom.compass_heading;
+	}
+}
+
+}
+
+
+#endif
---
+++ var_updated.c
@@ -0,0 +1,170 @@
+#ifndef __VAR__
+#define __VAR__
+
+/* -------------------------
+Fixed Global Variables
+---------------------------*/
+//Dimensions in cm
+float XMIN = 0.0;
+float YMIN = 0.0;
+float XMAX = 120.0;
+float YMAX = 240.0;
+float distanceThreshold = 30.0;
+
+/* -------------------------
+Robot Odometry
+---------------------------*/
+typedef struct{
+	float X;       	//in cm
+	float Y;        //in cm
+	float compass_heading; //in degrees
+	float heading;  //wrt to x-axis, in degrees
+	float w_left;   //in rad/s
+	float w_right;  //in rad/s
+	float v_X;      //in cm/s
+	float v_Y;      //in cm/s
+	float v;        //in cm/s
+	float w;        //in rad/s
+}Odom;
+Odom odom;
+
+/* -------------------------
+Robot Structure
+---------------------------*/
+typedef struct{
+	float length;
+	float width;
+	float wheel_radius;
+}Body;
+Body body;
+
+/* -------------------------
+Sensor Reading States
+---------------------------*/
+//Power Switch Logic State
+int Power_Switch_ON = 0;
+int Power_Switch_OFF = 1;
+//Ball Limit Switch Logic State
+int Ball_Limit_Switch_CONTACT = 0;
+int Ball_Limit_Switch_NO_CONTACT = 1;
+//Back Limit Switches Logic State
+int Back_Limit_Switch_CONTACT = 0;
+int Back_Limit_Switch_NO_CONTACT = 1;
+
+/* -------------------------
+Functional States
+---------------------------*/
+int ball_found = 0; //if ball is detected by IR sensor
+int ball_collected = 0;	//number of balls in collection area
+int ball_caught = 0; //if ball is detected by ball limit switch
+int roll_forward = 1;
+int boundary = 0;   //0: bottom, 1: left, 2: top, 3: right
+int phase = 0; 		//0: move to 2/3, 1: subsequent search
+int counter = 0;
+/* -------------------------
+Timing
+---------------------------*/
+int ball_caught_t;   //to track when the ball is caught and reverse roller for 1.5 seconds
+int moving_back_t;   //just for testing
+float count_period_left, count_period_right;	//to determine angular speed of both wheels (used in encoder.c)
+
+/* -------------------------
+Functions
+---------------------------*/
+
+/* Returns readings from respective IR Sharp Sensors
+Sharp 1 (10-80): Best range: 15-50cm (+- 2cm) distance = 27.534 * pow(volt , -1.207)
+Sharp 2 (10-80) : Best range: 10-55cm (+- 2cm) distance = 25.1 * pow(volt , -0.904)
+Sharp 3 (10-80): Best range: 10-30cm (+- 2cm) distance = 27.534 * pow(volt , -1.207)
+Sharp 4 (4-30): Best range: 4-20cm (+- 1cm) distance = 15.02 * pow(volt1 , -1.286) */
+int leftSensorReading(){
+	//Sharp 2
+	float volt = (float)SensorValue[leftIRSensor]/4096*5;
+	float distance = 25.1 * pow(volt , -0.904);
+	return distance;
+}
+
+int rightSensorReading(){
+	//Sharp 3
+	float volt = (float)SensorValue[rightIRSensor]/4096*5;
+	float distance = 27.534 * pow(volt , -1.207);
+	return distance;
+}
+
+int topSensorReading(){
+	//Sharp 1
+	float volt = (float)SensorValue[topIRSensor]/4096*5;
+	//TODO: update equation
+	float distance = 27.534 * pow(volt , -1.207);
+	return distance;
+}
+
+/* Controlling motor speeds for left and right wheels
+'leftMotorSpeed' and 'rightMotorSpeed' control speed of each motor
+'dir' controls the positive and negative signs for motor speeds to
+determine direction of motion-
+'f': forward
+'l': rotate counter-clockwise on the spot
+'r': rotate clockwise on the spot
+'b': backward */
+void moveMotor(float leftMotorSpeed, float rightMotorSpeed, char dir){
+	switch (dir){
+	case 'f':
+		motor[leftMotor] = (int)(leftMotorSpeed*127);
+		motor[rightMotor] = (int)(rightMotorSpeed*127);
+		break;
+	case 'l':
+		motor[leftMotor] = - (int)(leftMotorSpeed*127);
+		motor[rightMotor] = (int)(rightMotorSpeed*127);
+		break;
+	case 'r':
+		motor[leftMotor] = (int)(leftMotorSpeed*127);
+		motor[rightMotor] = - (int)(rightMotorSpeed*127);
+		break;
+	case 'b':
+		motor[leftMotor] = - (int)(leftMotorSpeed*127);
+		motor[rightMotor] = - (int)(rightMotorSpeed*127);
+		break;
+	}
+}
+
+float compass(void){
+	int num;
+	num = SensorValue[compass_MSB]*pow(2,3) + SensorValue[compass_Bit2]*pow(2,2) + SensorValue[compass_Bit3]*2 + SensorValue[compass_LSB];
+	switch(num){
+	case 7: return 0; //W
+		break;
+	case 3: return 45; //SW
+		break;
+	case 11: return 90; //S
+		break;
+	case 9: return 135; //SE
+		break;
+	case 13: return 180;  //E
+		break;
+	case 12: return 225; //NE
+		break;
+	case 14: return 270; //N
+		break;
+	case 6: return 315; //NW
+		break;
+	}
+
+	return -1;
+}
+
+//Initialise struct variables
+void initialise(){
+	//if(starting_pos = 0) odom.X = 40;
+	//else odom.X = 80;
+	odom.X = 30;
+	odom.Y = 15;
+	odom.compass_heading = compass(); //To do
+	odom.heading = 90.0;
+	body.length = 30.0;
+	body.width = 30.0;
+	body.wheel_radius = 3.5;
+}
+
+
+#endif
